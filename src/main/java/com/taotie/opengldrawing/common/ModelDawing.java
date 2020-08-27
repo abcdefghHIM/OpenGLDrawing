87eb24965a7a76a413967e9eedbf84f555db9300
abcdefghHIM <1404615410@qq.com>
2020-08-27 10:55:00 +0800

    first commit

---
+++ src/main/java/com/taotie/opengldrawing/common/ModelDawing.java
@@ -0,0 +1,12 @@
+package com.taotie.opengldrawing.common;
+
+import org.lwjgl.opengl.GL11;
+
+public class ModelDawing {
+	public void DrawingB()
+	{
+		GL11.glPushMatrix();
+		
+		GL11.glPopMatrix();
+	}
+}
