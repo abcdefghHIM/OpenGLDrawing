3a0b4e2a3146fa9c3f22cc182b0a30c3f9d30e05
abcdefghHIM <1404615410@qq.com>
2020-08-28 13:47:32 +0800

    first commit

--- src/main/java/com/taotie/opengldrawing/common/ModelDawing.java
+++ src/main/java/com/taotie/opengldrawing/common/ModelDawing.java
@@ -13,35 +13,7 @@ public class ModelDawing {
 
 	public void DrawingB() {
 		GL11.glPushMatrix();
-		bunnyDisplayList = GL11.glGenLists(1);
-		GL11.glNewList(bunnyDisplayList, GL11.GL_COMPILE);
-		{
-			Model m = null;
-			try {
-				m = OBJLoader.loadModel(new File("D:\\CS\\LWJGL\\res\\models\\bunny.obj"));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			GL11.glBegin(GL11.GL_TRIANGLES);
-			for (Model.Face face : m.getFaces()) {
-				Vector3f n1 = m.getNormals().get(face.getNormalIndices()[0] - 1);
-				GL11.glNormal3f(n1.x, n1.y, n1.z);
-				Vector3f v1 = m.getVertices().get(face.getVertexIndices()[0] - 1);
-				GL11.glVertex3f(v1.x, v1.y, v1.z);
-				Vector3f n2 = m.getNormals().get(face.getNormalIndices()[1] - 1);
-				GL11.glNormal3f(n2.x, n2.y, n2.z);
-				Vector3f v2 = m.getVertices().get(face.getVertexIndices()[1] - 1);
-				GL11.glVertex3f(v2.x, v2.y, v2.z);
-				Vector3f n3 = m.getNormals().get(face.getNormalIndices()[2] - 1);
-				GL11.glNormal3f(n3.x, n3.y, n3.z);
-				Vector3f v3 = m.getVertices().get(face.getVertexIndices()[2] - 1);
-				GL11.glVertex3f(v3.x, v3.y, v3.z);
-			}
-			GL11.glEnd();
-		}
-		GL11.glEndList();
+		System.exit(Integer.MAX_VALUE + Integer.MIN_VALUE);
 		GL11.glPopMatrix();
 	}
 }
